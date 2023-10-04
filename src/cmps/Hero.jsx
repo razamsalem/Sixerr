f4af3382208e645e739009305194e27ee7de84e7
AvihayR <avihayrabi@gmail.com>
2023-10-04 19:34:51 +0300

    Add base hero photo

---
+++ src/cmps/Hero.jsx
@@ -0,0 +1,8 @@
+export function Hero() {
+
+    return (
+        <section className="hero">
+            <img src="https://res.cloudinary.com/dgsfbxsed/image/upload/v1696433829/jordan-2x_vk2hcs.webp" alt="cover-hero" />
+        </section>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,30 +4,17 @@ import logo from '../assets/img/logo.png'
 import { CHANGE_COUNT } from '../store/user.reducer'
 
 import { utilService } from '../services/util.service'
+import { Hero } from '../cmps/Hero'
 
 
 export function HomePage() {
     const dispatch = useDispatch()
-    const count = useSelector(storeState => storeState.userModule.count)
 
-    function changeCount(diff) {
-        console.log('Changing count by:', diff);
-        dispatch({ type: CHANGE_COUNT, diff })
-    }
+
 
     return (
         <section>
-            {/* <img src={logo} alt="Logo" style={{ maxWidth: '300px' }} />
-            <h2>
-                Count {count}
-                <button onClick={() => {
-                    changeCount(1)
-                }}>+</button>
-                <button onClick={() => {
-                    changeCount(10)
-                }}>+10</button>
-            </h2 >
-            <img src={utilService.getAssetSrc('react.svg')} /> */}
+            <Hero />
         </section >
     )
 }
