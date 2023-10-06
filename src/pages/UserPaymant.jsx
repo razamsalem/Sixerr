d6fa9783426ca1f7d558ce1486ad15b895b4c03f
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-10-06 15:55:07 +0300

    F/working of the functuality of getting an order

--- src/pages/UserPaymant.jsx
+++ src/pages/UserPaymant.jsx
@@ -1,13 +1,23 @@
 import { useSelector } from "react-redux"
 import cards from "../assets/img/credit-cards.svg"
 import { RadioButton } from "../cmps/RadioButton"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { orderService } from "../services/order.service.local"
 export  function  UserPayment() {
 
-    const user = useSelector((storeState) => storeState.userModule.loggedinUser)
+    const user = useSelector((storeState) => storeState.userModule.watchedUser)
     const username = user?.username || 'tzvia123'
-    const [radioOptions,setRadioOptions] = useState({visa: false, paypal : false})
-
+    const [order,setOrder] = useState(orderService.getEmptyOrder());
+    // const name = user?. || 'tzvia123'
+    // const lastName = user?.username || 'tzvia123'
+    const [radioOptions,setRadioOptions] = useState({visa: true, paypal : false})
+    useEffect(async ()=>{
+        try {
+            const orderToSave = await saveOrder({...order})
+          } catch (err) {
+              console.log('Cannot add toy', err);
+          }
+    },[order])
     function onChangeRadioButton(ev) {
         console.log(ev.target.name);
         if(ev.target.name == 'visa'){
@@ -17,6 +27,13 @@ export  function  UserPayment() {
             setRadioOptions({visa: false, paypal : true})
         }
     }
+
+    async function onAddOrder(ev) {
+        const gig = await orderService.getById(params.gigId)
+        setOrder((prevOrder)=>({...prevOrder, buyer: user, seller: gig.owner, gig}))
+        
+    }       
+
     return(
         <section className="payment-container">
 
@@ -55,18 +72,18 @@ export  function  UserPayment() {
                                     <label htmlFor=""><h6>Card Number</h6></label>
                                     <div className="credit-card-input-wrapper">
                                         <img src="https://fiverr-res.cloudinary.com/image/upload/f_png,q_auto/v1/attachments/generic_asset/asset/2496be9dfb5983d9de91630d83bb21e0-1682945799754/generic.svg" alt="credit-card-icon" className="card-logo"/>
-                                        <input type="text" placeholder="0000 0000 0000 0000"/>
+                                        <input type="text" placeholder="0000 0000 0000 0000" value= '5326-1011-8754-8979'/>
                                     </div>
                                 </div>
 
                                 <div className="card">
                                     <span className="expiration-date">
                                         <label htmlFor=""><h6>Expiration date</h6></label>
-                                        <input type="text" name="" id="" className="input" placeholder="MM/YY"/>
+                                        <input type="text" name="" id="" className="input" placeholder="MM/YY" value='03/28'/>
                                     </span>
                                     <div className="security-code">
                                         <label htmlFor=""><h6>Security code</h6></label>
-                                        <input type="text" name="" id="" className="input"/>
+                                        <input type="text" name="" id="" className="input" value='345'/>
                                     </div>
                                 </div>
 
@@ -111,11 +128,11 @@ export  function  UserPayment() {
                                 <div className="card">
                                     <div className="first-name">
                                         <label htmlFor=""><h6>First name</h6></label>
-                                        <input type="text" name="" id="" className="input"/>
+                                        <input type="text" name="" id="" className="input" value={username}/>
                                     </div>
                                     <div className="last-name">
                                         <label htmlFor=""><h6>Last name</h6></label>
-                                        <input type="text" name="" id="" className="input"/>
+                                        <input type="text" name="" id="" className="input" value='izhakov'/>
                                     </div>
                                 </div>
 
@@ -132,10 +149,12 @@ export  function  UserPayment() {
                             checked={radioOptions.paypal}/>
                   <img src="https://finderr.onrender.com/static/media/paypal.2268abba910e45e692258282d2801b10.svg" alt="paypal" className="paypal-logo"/>
                 </section>
+
+                <button onClick={onAddOrder}>finish-order</button>
             </section>
             
             
-
+        
         </section>
 
     
