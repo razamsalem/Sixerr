ad66743e7cd735c8fb6f013d864bc4dd28edd36a
AvihayR <avihayrabi@gmail.com>
2023-10-08 16:01:26 +0300

    Add base styling OrderIndex

--- src/pages/OrderIndex.jsx
+++ src/pages/OrderIndex.jsx
@@ -4,8 +4,8 @@ import { OrderList } from "../cmps/OrderList"
 export function OrderIndex() {
     const orders = useSelector(storeState => storeState.orderModule.orders)
     return (
-        <section className="orders-page">
-            <h1>My orders</h1>
+        <section className="orders-page main-layout full">
+            <h1 className="orders-heading">Purchased orders<span className="dot">.</span></h1>
             {orders.length && <OrderList orders={orders} mode={'buyer'} />}
         </section>
     )
