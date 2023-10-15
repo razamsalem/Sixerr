1cb4890d0f491480617af4affc410598f2fdde6d
AvihayR <avihayrabi@gmail.com>
2023-10-15 15:32:38 +0300

    Change gig-preview styling

--- src/cmps/GigPreview.jsx
+++ src/cmps/GigPreview.jsx
@@ -8,14 +8,13 @@ export function GigPreview({ gig, onRemoveGig, onUpdateGig }) {
 
         <li className="gig-preview" key={gig._id}>
 
-            <div>
+            <div className="img-container">
                 <Link to={`/gig/${gig._id}`}>
                     <img className="gig-img" src={gig.imgUrls ? gig.imgUrls[0] : defaultImgUrl} alt="gig-img" />
                 </Link>
             </div>
 
             <div className="flex owner-details">
-                {/* {console.log(gig)} */}
                 <div className="flex owner-details-1">
                     <img src={gig.owner.imgUrl} alt="progile-img" className="owner-profile-img" />
                     <span className="owner-fullname">{gig.owner.fullname}</span>
@@ -30,7 +29,6 @@ export function GigPreview({ gig, onRemoveGig, onUpdateGig }) {
                         <svg width="16" height="15" viewBox="0 0 16 15" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" clipRule="evenodd" d="M16 5.81285C16 5.98299 15.875 6.14367 15.75 6.26654L12.2596 9.61248L13.0865 14.3384C13.0962 14.4045 13.0962 14.4612 13.0962 14.5274C13.0962 14.7732 12.9808 15 12.7019 15C12.5673 15 12.4327 14.9527 12.3173 14.8866L8 12.656L3.68269 14.8866C3.55769 14.9527 3.43269 15 3.29808 15C3.01923 15 2.89423 14.7732 2.89423 14.5274C2.89423 14.4612 2.90385 14.4045 2.91346 14.3384L3.74038 9.61248L0.240385 6.26654C0.125 6.14367 0 5.98299 0 5.81285C0 5.5293 0.298077 5.41588 0.538462 5.37807L5.36539 4.68809L7.52885 0.387524C7.61539 0.207939 7.77885 0 8 0C8.22115 0 8.38462 0.207939 8.47115 0.387524L10.6346 4.68809L15.4615 5.37807C15.6923 5.41588 16 5.5293 16 5.81285Z"></path>
                         </svg>
-                        {/* <img src="src/assets/img/star.svg" alt="star-svg" className="star" /> */}
                     </span>
                     <span className="owner-rate">{gig.owner.rate}</span>
                     <span className="owner-number-rates">(137)</span>
