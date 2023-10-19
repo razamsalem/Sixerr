f2e29774f7f5f046aeb6152d88491137ccea0165
AvihayR <avihayrabi@gmail.com>
2023-10-19 14:10:38 +0300

    Add closing dropDownMenu logic

--- src/cmps/DropdownBtn.jsx
+++ src/cmps/DropdownBtn.jsx
@@ -1,11 +1,26 @@
-import { DropdownMenu } from "./DropdownMenu"
-import { useState } from "react"
+import React, { useState, useEffect, useRef } from "react";
+import { DropdownMenu } from "./DropdownMenu";
 
 export function DropdownBtn({ icon, children = '' }) {
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false);
+    const buttonRef = useRef(null)
+
+    function closeDropdown(event) {
+        if (buttonRef.current && !buttonRef.current.contains(event.target)) {
+            setIsOpen(false)
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("click", closeDropdown)
+
+        return () => {
+            document.removeEventListener("click", closeDropdown)
+        }
+    }, [])
 
     return (
-        <a className="drop-down-btn" onClick={() => { setIsOpen(!isOpen) }}>
+        <a className="drop-down-btn" onClick={() => { setIsOpen(!isOpen) }} ref={buttonRef}>
             {<i className="fa-solid fa-circle-chevron-down"></i>}
             {isOpen && <DropdownMenu items={children} />}
         </a>
