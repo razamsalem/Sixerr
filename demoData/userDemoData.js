8a2a69edd88ebb9bc36c083c9a47be31536ba639
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-10-22 20:11:23 +0300

    adding demo data for review

--- demoData/userDemoData.js
+++ demoData/userDemoData.js
@@ -8,18 +8,36 @@ const demoUsers = [
     level: "basic/premium",
     desc: "Are you looking for an expert logo designer to design your logo? If your answer is yes, then I welcome you and you are at the right place.",
     isSeller: true,
+    location: "Germany",
     reviews: [
       {
-        "id": "madeId",
+        "id": "r101",
         "gig": "{optional-mini-gig}",
         "txt": "Very kind and works fast",
         "rate": 4,
+        "createdAt" : 1697993927386,
+        "reviewImgUrl": null,
+        "by": {
+          "_id": "u207",
+          "fullname": "James Q",
+          "imgUrl": "/img/img2.jpg",
+          "location" : "Italy"
+        }
+      },
+      {
+        "id": "r102",
+        "gig": "{optional-mini-gig}",
+        "txt": "Very kind and works fast",
+        "rate": 2,
+        "createdAt" : 1697993927389,
+        "reviewImgUrl": null,
         "by": {
           "_id": "u102",
-          "fullname": "user2",
-          "imgUrl": "/img/img2.jpg"
+          "fullname": "Jhon I",
+          "imgUrl": "https://res.cloudinary.com/de2rdmsca/image/upload/v1696177501/samples/people/boy-snow-hoodie.jpg",
+          "location" : "United States"
         }
-      }
+      },
     ],
   },
   {
@@ -35,14 +53,17 @@ const demoUsers = [
     lang: ['English', 'Hebrew'],
     reviews: [
       {
-        "id": "madeId",
+        "id": "r103",
         "gig": "{optional-mini-gig}",
         "txt": "Very kind and works fast",
-        "rate": 4,
+        "rate": 2,
+        "createdAt" : 1697993927388,
+        "reviewImgUrl": null,
         "by": {
-          "_id": "u102",
-          "fullname": "user2",
-          "imgUrl": "/img/img2.jpg"
+          "_id": "u101",
+          "fullname": "Taylor P",
+          "imgUrl": "https://res.cloudinary.com/de2rdmsca/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1696177526/cld-sample.jpg",
+          "location" : "United States",
         }
       }
     ],
@@ -60,14 +81,17 @@ const demoUsers = [
     lang: ['Italian', 'Chinese', 'English', 'Spanish'],
     reviews: [
       {
-        "id": "madeId",
+        "id": "r104",
         "gig": "{optional-mini-gig}",
         "txt": "Very kind and works fast",
-        "rate": 4,
+        "rate": 3,
+        "createdAt" : 1697993927340,
+        "reviewImgUrl": null,
         "by": {
-          "_id": "u102",
-          "fullname": "user2",
-          "imgUrl": "/img/img2.jpg"
+          "_id": "u208",
+          "fullname": "Liri C",
+          "imgUrl": "https://res.cloudinary.com/de2rdmsca/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1696589336/MV5BZDJhYzdkMTUtMTg2MS00ZTM5LWI1OGUtYTY5MzY4NDM3NThjXkEyXkFqcGdeQXVyMjQwMDg0Ng_._V1__okvske.jpg",
+          "location" : "Greece",
         }
       }
     ],
@@ -85,14 +109,17 @@ const demoUsers = [
     lang: ['English', 'Spanish', 'Hebrew'],
     reviews: [
       {
-        "id": "madeId",
+        "id": "r105",
         "gig": "{optional-mini-gig}",
         "txt": "Very kind and works fast",
-        "rate": 4,
+        "rate": 1,
+        "createdAt" : 1697993927315,
+        "reviewImgUrl": null,
         "by": {
           "_id": "u102",
-          "fullname": "user2",
-          "imgUrl": "/img/img2.jpg"
+          "fullname": "Jhon I",
+          "imgUrl": "https://res.cloudinary.com/de2rdmsca/image/upload/v1696584327/qcw1ydkltzywb3g4s5ty.jpg",
+          "location" : "United States",
         }
       }
     ],
