c9a13a298439fc9405e57624cd2d3dd395ba4621
AvihayR <avihayrabi@gmail.com>
2023-10-22 20:40:50 +0300

    Remove PWA Service workers

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
+// import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+// import { HashRouter as Router } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { HashRouter as Router } from 'react-router-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import { store } from './store/store'
 import { RootCmp } from './RootCmp'
 import './assets/styles/main.scss'
@@ -22,4 +22,4 @@ root.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register()
+// serviceWorkerRegistration.register()
