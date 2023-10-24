cbf0b23d88980627045cafcb8353b9f9f1ead1dc
Raz <amsalemraz10@gmail.com>
2023-10-24 12:39:12 +0300

    /F/Dashboard/Buttons change the content accordingly

--- src/cmps/DashboardModal.jsx
+++ src/cmps/DashboardModal.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import AreaResChart from "./AreaResponsiveChart"
 import { DoughnutChart } from "./DoughnutChart"
 import { NeedleChart } from "./NeedleChart"
@@ -5,6 +6,8 @@ import { OrderList } from "./OrderList"
 import StatsRadarChart from "./RadarChart"
 
 export function DashboardModal({ watchedUser, orders, loggedUser, closeDashboard, handleBackgroundClick }) {
+
+    const [activeBtn, setActiveBtn] = useState('home')
 
     return (
         <section className="modal-background" onClick={handleBackgroundClick}>
@@ -14,31 +17,58 @@ export function DashboardModal({ watchedUser, orders, loggedUser, closeDashboard
                 <div className="nav-content">
                     <h1 className="logo">sixerr<span className='dot'>.</span></h1>
                     <div className="buttons flex column">
-                        <button> <span className="fa-solid fa-house icon"></span> Home</button>
-                        <button> <span className="fa-solid fa-square-poll-vertical icon"></span> Dashboard</button>
-                        <button> <span className="fa-solid fa-chart-pie icon"></span> Insights</button>
+
+                        <button
+                            className={activeBtn === 'home' ? 'active' : ''}
+                            onClick={() => setActiveBtn('home')}
+                        >
+                            <span className="fa-solid fa-house icon"></span> Home
+                        </button>
+
+                        <button
+                            className={activeBtn === 'dashboard' ? 'active' : ''}
+                            onClick={() => setActiveBtn('dashboard')}
+                        >
+                            <span className="fa-solid fa-square-poll-vertical icon"></span> Dashboard
+                        </button>
+
+                        <button
+                            className={activeBtn === 'insights' ? 'active' : ''}
+                            onClick={() => setActiveBtn('insights')}
+                        >
+                            <span className="fa-solid fa-chart-pie icon"></span> Insights
+                        </button>
+
                     </div>
                 </div>
+
                 <div className="space"></div>
                 <div className="dashboard-content">
-                    <h3 className="header">Home</h3>
-                    <div className="front-charts flex">
-                        <div className="radar">
-                            <StatsRadarChart />
-                        </div>
-                        <div className="needle">
-                            <h3>Friendship Status <span className="fa-regular fa-circle-question icon" title="if you reach the red zone you will not be able to continue selling!"></span> </h3>
-                            <p>Friendship status depends on the review of your clients.</p>
-                            <NeedleChart />
-                        </div>
-                    </div>
-                    <main className="graph">
-                        <h4 className="sub-header">This week's sales</h4>
-                        <AreaResChart />
-                    </main>
-                    {/* <div className="activities">
-                        <h4 className="sub-header">Recent activities</h4>
-                    </div> */}
+                    {activeBtn === 'home' && (
+                        <>
+                            <h3 className="header">Home</h3>
+                            <div className="front-charts flex">
+                                <div className="radar">
+                                    <StatsRadarChart />
+                                </div>
+                                <div className="needle">
+                                    <h3>Friendship Status <span className="fa-regular fa-circle-question icon" title="If you reach the red zone you will not be able to continue selling!"></span> </h3>
+                                    <p>Friendship status depends on the review of your clients.</p>
+                                    <NeedleChart />
+                                </div>
+                            </div>
+                            <main className="graph">
+                                <h4 className="sub-header">This week's sales</h4>
+                                <AreaResChart />
+                            </main>
+                            {/* 
+                                <div className="activities">
+                                  <h4 className="sub-header">Recent activities</h4>
+                                 </div>
+                            */}
+                        </>
+                    )}
+
                 </div>
                 <div className="space2"></div>
                 <div className="user-info">
