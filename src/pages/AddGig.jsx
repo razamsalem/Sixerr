45b43e20c2603ad5ff81c8435fa2dd11e795fd26
AvihayR <avihayrabi@gmail.com>
2023-10-24 12:02:25 +0300

    Add AddGig page and route

---
+++ src/pages/AddGig.jsx
@@ -0,0 +1,9 @@
+export function AddGig() {
+
+    return (
+        <section className="add-gig">
+            <h1 className="heading">Add a gig</h1>
+            <h2 className="sub-heading">Fill the required information and share your details about your new gig</h2>
+        </section>
+    )
+}
