9e196d659ff475674d31c364a820943f1c6903dd
AvihayR <avihayrabi@gmail.com>
2023-10-25 13:54:59 +0300

    Add tags to gig.service categories

--- src/services/gig.service.local.js
+++ src/services/gig.service.local.js
@@ -10,7 +10,7 @@ _createGigs()
 
 const categories = [
     { category: "Graphics & Design", tags: ['Logo Design', 'Website Design', 'App Design', 'UX Design', 'Landing Page Design', 'Icon Design'] },
-    { category: "Programming & Tech", tags: ['Website Development', 'Business Websites', 'E-Commerce Development', 'Landing Pages', 'Web Applications', 'HTML & CSS Developers', 'JavaScript Developers'] },
+    { category: "Programming & Tech", tags: ['Website Development', 'Business Websites', 'E-Commerce Development', 'Landing Pages', 'Web Applications', 'HTML & CSS Developers', 'JavaScript Developers', 'AI Services', 'Wordpress'] },
     { category: "Digital Marketing", tags: ["Search Engine Optimization (SEO)", "Search Engine Marketing (SEM)", "Local SEO", "E-Commerce SEO", "Video SEO", "Email Marketing", "Guest Posting", "Affiliate Marketing"] },
     { category: "Video & Animation", tags: ["Video Editing", "Visual Effects", "Video Art", "Logo Animation", "Character Animation", "Animated GIFs"] },
     { category: "Writing & Translation", tags: ["Articles & Blog Posts", "Content Strategy", "Proofreading & Editing", "AI Content Editing", "Book Editing", "Beta Reading", "Writing Advice", "Career Writing", "Resume Writing", "Cover Letters", "LinkedIn Profiles"] },
