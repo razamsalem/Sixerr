2d4fcca678d2191488c9ae0932d6e96700e7c319
AvihayR <avihayrabi@gmail.com>
2023-10-26 16:16:49 +0300

    Add base Category-Slider

--- src/cmps/CategoryNav.jsx
+++ src/cmps/CategoryNav.jsx
@@ -1,19 +1,41 @@
+import { useRef, useState } from "react"
 import { Link } from "react-router-dom"
 
 export function CategoryNav({ categories, setFilterBy, globalFilterBy, subHeaderPosition }) {
+    const firstChild = useRef()
+    const [marginLeft, setMarginLeft] = useState(0)
+
+    if (firstChild.current) console.dir(firstChild.current.style)
 
     function onSetFilterBy(category) {
         setFilterBy({ ...globalFilterBy, tags: '', category })
     }
 
+    function onSlideRight() {
+        setMarginLeft(margin => margin - 25)
+    }
+
+    function onSlideLeft() {
+        setMarginLeft(margin => margin + 25)
+    }
+
     return (
         <div className={`${subHeaderPosition} sub-header-container main-layout full animate__animated animate__flipInX`}>
             <nav className="category-header">
+                <button className="nav-btn left" onClick={onSlideLeft}>
+                    <i className="fa-solid fa-chevron-left"></i>
+                </button>
                 <ul className="categories-container">
-                    {categories.map(c => <Link to={'/gig'} key={c.category} onClick={() => { onSetFilterBy(c.category) }} className={`category-link ${globalFilterBy.category === c.category ? 'selected' : ''}`}>{c.category}</Link>)}
+                    {categories.map(c => {
+                        const isFirstChild = c.category === categories[0].category
+                        return <Link style={isFirstChild ? { 'marginLeft': `${marginLeft}%` } : {}} ref={isFirstChild ? firstChild : null} to={'/gig'} key={c.category} onClick={() => { onSetFilterBy(c.category) }} className={`category-link ${globalFilterBy.category === c.category ? 'selected' : ''}`}>{c.category}</Link>
+                    })}
                 </ul>
+                <button className="nav-btn right" onClick={onSlideRight}>
+                    <i className="fa-solid fa-chevron-right"></i>
+                </button >
             </nav>
-        </div>
+        </div >
 
     )
 }
