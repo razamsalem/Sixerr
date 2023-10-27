aac8c5f3dcc42c0b4d135f5983b07df798a62a26
Raz <amsalemraz10@gmail.com>
2023-10-27 14:14:50 +0300

    Packages buttons Pixel Perfect

--- src/cmps/CallToAction.jsx
+++ src/cmps/CallToAction.jsx
@@ -2,6 +2,7 @@ import timeImg from '../assets/img/time.svg';
 import checkImg from '../assets/img/check.svg';
 import { Link, useLocation } from 'react-router-dom';
 import { useState } from 'react';
+import { utilService } from '../services/util.service';
 
 export function CallToAction({ gig, isPurchase = false, onPurchaseOrder }) {
     const { price, daysToMake, packages } = gig
@@ -14,56 +15,56 @@ export function CallToAction({ gig, isPurchase = false, onPurchaseOrder }) {
     }
 
     return (
-        <article className="call-to-action">
-
-            <div className="package-options">
+        <section className="cta-container">
+            <div className="package-options flex">
                 {Object.keys(packages).map((packageKey) => (
                     <button
                         key={packageKey}
                         className={`package ${selectedPackage === packageKey ? 'selected' : ''}`}
                         onClick={() => handlePackageChange(packageKey)}
                     >
-                        {packageKey}
+                        {utilService.capitalizeFirstLetter(packageKey)}
                     </button>
                 ))}
             </div>
+            <article className="call-to-action">
+                <section className="package-heading">
+                    <h1 className='package'>{selectedPackage.charAt(0).toUpperCase() + selectedPackage.slice(1)}</h1>
+                    <h2 className='price'>US $ {price}</h2>
+                </section>
 
-            <section className="package-heading">
-            <h1 className='package'>{selectedPackage.charAt(0).toUpperCase() + selectedPackage.slice(1)}</h1>
-                <h2 className='price'>US $ {price}</h2>
-            </section>
+                <h3 className='package-desc'>{desc}</h3>
 
-            <h3 className='package-desc'>{desc}</h3>
+                <span className="days-container">
+                    <img className='icon time' src={timeImg} alt="time-icon" />
+                    <h3>{daysToMake} Days Delivery</h3>
+                </span>
 
-            <span className="days-container">
-                <img className='icon time' src={timeImg} alt="time-icon" />
-                <h3>{daysToMake} Days Delivery</h3>
-            </span>
+                <ul className="feature-list">
+                    {features.map((feature) => (
+                        <span key={feature} className='feature-container'>
+                            <img className='icon check' src={checkImg} alt="time-icon" />
+                            {feature}
+                        </span>
+                    ))}
+                </ul>
 
-            <ul className="feature-list">
-                {features.map((feature) => (
-                    <span key={feature} className='feature-container'>
-                        <img className='icon check' src={checkImg} alt="time-icon" />
-                        {feature}
-                    </span>
-                ))}
-            </ul>
-
-            {isPurchase ?
-                <div className='purchase-btn-container'>
-                    <button onClick={onPurchaseOrder} className='btn continue'>Pay in USD</button>
-                    <span>
-                        <i className="fa-solid fa-lock"></i>  SSL Secure Payment
-                    </span>
-                </div>
-                :
-                <Link to={`${pathname}/checkout`} className='btn continue'>
-                    Continue
-                </Link>
-            }
-            {!isPurchase && <div className='compare'>
-                <a className='compare-link'>Compare packages</a>
-            </div>}
-        </article>
+                {isPurchase ?
+                    <div className='purchase-btn-container'>
+                        <button onClick={onPurchaseOrder} className='btn continue'>Pay in USD</button>
+                        <span>
+                            <i className="fa-solid fa-lock"></i>  SSL Secure Payment
+                        </span>
+                    </div>
+                    :
+                    <Link to={`${pathname}/checkout`} className='btn continue'>
+                        Continue
+                    </Link>
+                }
+                {!isPurchase && <div className='compare'>
+                    <a className='compare-link'>Compare packages</a>
+                </div>}
+            </article>
+        </section>
     )
 }
