5ce05bf3b51390062f2c7e12ad12f09c25589d19
AvihayR <avihayrabi@gmail.com>
2023-10-31 20:13:41 +0200

    Change default user img

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -56,7 +56,7 @@ async function login(userCred) {
 }
 
 async function signup(userCred) {
-    if (!userCred.imgUrl) userCred.imgUrl = 'https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png'
+    if (!userCred.imgUrl) userCred.imgUrl = 'https://res.cloudinary.com/dgsfbxsed/image/upload/v1698775961/user-96_hcptba.png'
     const user = await storageService.post('user', userCred)
     // const user = await httpService.post('auth/signup', userCred)
     return saveLocalUser(user)
