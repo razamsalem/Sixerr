8db815c847bad991f4d0afc490e31b8f7654b178
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-10-31 15:14:53 +0200

    F/chnging the filterby structure to have tags in array, working on the service options

--- src/cmps/PopularTags.jsx
+++ src/cmps/PopularTags.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom"
+import { gigService } from "../services/gig.service.local"
 
 export function PopularTags({ globalFilterBy, setFilterBy }) {
     const tags = ['Website Design', 'Wordpress', 'Logo Design', 'AI Services']
-
+    const categories = {'Website Design':'Graphics+%26+Design','Wordpress':'Programming+%26+Tech','Logo Design':'Graphics+%26+Design','AI Services':'Programming+%26+Tech'}
     return (
         <div className="popular">
             Popular:
             <ul>
-                {tags.map(tag => <li key={tag}><Link to={`/gig?tags=${tag}`}  className="btn">{tag}</Link></li>)}
+                {tags.map(tag => <li key={tag}><Link to={`/gig?&category=${categories[tag]}`}  className="btn" onClick={()=>setFilterBy({...globalFilterBy,tags:[tag]})}>{tag}</Link></li>)}
             </ul>
         </div>
     )
