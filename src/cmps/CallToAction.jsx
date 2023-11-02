0a4f46608d8a3fb4ea3832389edf74bab81eebe2
Raz <amsalemraz10@gmail.com>
2023-11-02 09:49:58 +0200

    Cta modal pixel perfect

--- src/cmps/CallToAction.jsx
+++ src/cmps/CallToAction.jsx
@@ -1,25 +1,29 @@
 import timeImg from '../assets/img/time.svg'
+import closeImg from '../assets/img/sixerr-x.svg'
 import checkImg from '../assets/img/check.svg'
+import packImg from '../assets/img/package.svg'
+import revisionImg from '../assets/img/revisions.svg'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
 import { utilService } from '../services/util.service'
 import { useSelector } from 'react-redux'
 import { setUserModalOpen } from '../store/user.actions'
 import { showErrorMsg } from '../services/event-bus.service'
+import CtaModal from './CtaModal'
 const imgNotFound = 'https://res.cloudinary.com/dgsfbxsed/image/upload/v1698663092/defaultGigImg_vjtk9e.webp'
 
 
 export function CallToAction({ gig, isPurchase = false, onPurchaseOrder, openModal, selectedPack = 'basic', setPackage }) {
     const loggedUser = useSelector(storeState => storeState.userModule.user)
     const navigate = useNavigate()
-    const { price, daysToMake, packages, imgUrls } = gig
+    const { title, price, daysToMake, packages, imgUrls } = gig
     const [selectedPackage, setSelectedPackage] = useState(selectedPack)
-    const { title, packPrice, packDaysToMake, desc, features } = packages[selectedPackage]
+    const { title: packTitle, packPrice, packDaysToMake, desc, features } = packages[selectedPackage]
+    const [isModalOpen, setModalOpen] = useState(false)
     const { pathname } = useLocation()
 
     function handlePackageChange(packageKey) {
         setSelectedPackage(packageKey)
-        // setPackage(packageKey)
     }
 
     function goToCheckout() {
@@ -30,8 +34,74 @@ export function CallToAction({ gig, isPurchase = false, onPurchaseOrder, openMod
         }
     }
 
+    function openModal() {
+        document.documentElement.classList.add('modal-open')
+        setModalOpen(true)
+    }
+
+    function closeModal() {
+        document.documentElement.classList.remove('modal-open')
+        setModalOpen(false)
+    }
+
     return (
         <section className="cta-container">
+            <CtaModal isOpen={isModalOpen} onClose={closeModal} >
+                <section className="modal-content flex">
+                    <div className="header flex">
+                        <div className="header-content">Order options</div>
+                        <button onClick={closeModal}><img src={closeImg} alt="close modal" /></button>
+                    </div>
+                    <div className="modal-main">
+                        <div className="selected-order">
+                            <div className="pack flex">
+                                <span className="bold">{utilService.capitalizeFirstLetter(selectedPackage)}</span>
+                                <span>${packPrice}</span>
+                            </div>
+                            <div className="desc">
+                                <span className='bold'>{packTitle}</span>{title}
+                            </div>
+                        </div>
+                        <div className="order-info">
+                            <span className='bold'>Know your order</span>
+                            <div className="order">
+                                <div className="subtitle flex">
+                                    <span className='bolder'>${packPrice}</span>
+                                    <span className='small'>Single order</span>
+                                </div>
+
+                                <div className="info border-top flex">
+                                    <span className='icon'> <img src={packImg} alt="package icon" /> </span> <span className='level'>{`${utilService.capitalizeFirstLetter(selectedPackage)} package`} </span>
+                                </div>
+
+                                <div className="features">
+                                    <ul>
+                                        {features.map(feature => (
+                                            <li>{feature}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+
+                                <div className="info flex">
+                                    <span className='icon'> <img src={timeImg} alt="clock-icon" /> </span> <span className='level'>{packDaysToMake}-{packDaysToMake === 1 ? 'day' : 'days'} delivery</span>
+                                </div>
+
+                                <div className="info flex">
+                                    <span className='icon'> <img src={revisionImg} alt="revision-icon" /> </span> <span className='level'>Unlimited revisions</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <footer className='footer'>
+                        <div className="purchase-btn">
+                            <button>Continue (${packPrice})</button>
+                            <span className='footer-msg flex'>You won’t be charged yet</span>
+                        </div>
+                    </footer>
+                </section>
+            </CtaModal>
+
             <div className="package-options flex">
                 {Object.keys(packages).map((packageKey) => (
                     <button
@@ -52,11 +122,11 @@ export function CallToAction({ gig, isPurchase = false, onPurchaseOrder, openMod
                     <h2 className='price'><span className="price-font">$ {packPrice ? packPrice : price}</span></h2>
                 </section>
 
-                <h3 className='package-desc'><span className='package'>{title ? title : selectedPackage.charAt(0).toUpperCase() + selectedPackage.slice(1)} </span>{!isPurchase && desc}</h3>
+                <h3 className='package-desc'><span className='package'>{packTitle ? packTitle : selectedPackage.charAt(0).toUpperCase() + selectedPackage.slice(1)} </span>{!isPurchase && desc}</h3>
 
                 <span className="days-container">
                     <img className='icon clock' src={timeImg} alt="clock-icon" />
-                    <h3>{packDaysToMake ? packDaysToMake : daysToMake} Days Delivery</h3>
+                    <h3>{packDaysToMake ? packDaysToMake : daysToMake} {packDaysToMake === 1 ? 'Day' : 'Days'} Delivery</h3>
                 </span>
 
                 <ul className="feature-list">
@@ -76,7 +146,7 @@ export function CallToAction({ gig, isPurchase = false, onPurchaseOrder, openMod
                         </span>
                     </div>
                     :
-                    <a onClick={goToCheckout} className='btn continue'>
+                    <a onClick={openModal} className='btn continue'>
                         Continue
                         <i className="fa-solid fa-arrow-right"></i>
                     </a>
--- src/pages/GigDetails.jsx
+++ src/pages/GigDetails.jsx
@@ -11,9 +11,7 @@ import { userService } from "../services/user.service"
 import { showErrorMsg } from "../services/event-bus.service"
 import { BreadCrumbs } from "../cmps/BreadCrumbs"
 import LoadingCircle from "../cmps/LoadingCircle"
-import timeImg from '../assets/img/sixerr-x.svg'
 import starGrey from "../assets/img/star-grey.svg"
-import CtaModal from "../cmps/CtaModal"
 const defaultGigImg = 'https://res.cloudinary.com/dgsfbxsed/image/upload/v1698663092/defaultGigImg_vjtk9e.webp'
 const defaultUserImg = 'https://res.cloudinary.com/dgsfbxsed/image/upload/v1698663308/defaultUserImg_psy0oe.png'
 
@@ -23,9 +21,6 @@ export function GigDetails() {
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const [seller, setSeller] = useState(null)
-    const [isModalOpen, setModalOpen] = useState(false)
-    const [selectedLevel, setSelectedLevel] = useState('null')
-    const [selectedPackage, setSelectedPackage] = useState('basic')
     const userInfoRef = useRef(null)
     const userReviewRef = useRef(null)
     const defaultImgUrl = 'https://res.cloudinary.com/dgsfbxsed/image/upload/v1698161431/sixxer-logo_vseimk.png'
@@ -44,16 +39,6 @@ export function GigDetails() {
             showErrorMsg('Oops cannot load review')
             navigate('/')
         }
-    }
-
-    function openModal() {
-        document.documentElement.classList.add('modal-open')
-        setModalOpen(true)
-    }
-
-    function closeModal() {
-        document.documentElement.classList.remove('modal-open')
-        setModalOpen(false)
     }
 
     if (gig) loadUser()
@@ -90,85 +75,63 @@ export function GigDetails() {
 
     if (!gig || !seller) return <div className='loading'>{<LoadingCircle />}</div>
     return (
-        <>
-            <CtaModal isOpen={isModalOpen} onClose={closeModal} >
-                <section className="modal-content">
-                    <div className="header flex">
-                        <div className="header-content">Order options</div>
-                        <button onClick={closeModal}><img src={timeImg} alt="close modal" /></button>
-                    </div>
-                    <div className="modal-main">
-                        <div className="selected-order">
-                            <div className="pack flex">
-                                <span className="bold">{selectedPackage}</span>
-                                <span>$5</span>
+        <section className="gig-details full main-layout">
+            <BreadCrumbs category={gig.category} />
+            <CallToAction gig={gig} addToCart={addToCart} />
+            <section className="top-details container full main-layout">
+                <div className="owner-details-container">
+                    <h1 className="gig-title">{gig.title}</h1>
+                    <div className="profile-container">
+                        <img src={gig.owner.imgUrl} alt="owner-img" className="owner-profile-img-meduim" onClick={scrollToUserInfo} onError={e => e.currentTarget.src = defaultUserImg} />
+                        <div className="owner-details">
+                            <div className="user-container" onClick={scrollToUserInfo}>
+                                <h3 className="user-title">{gig.owner.fullname}</h3>
+                                <span className="username">@{seller.username}</span>
+                            </div>
+                            <div className="star-wrapper" onClick={scrollToReview}>
+                                <span className="star-svg">
+                                    <img src={starGrey} alt="star-svg" className="star" />
+                                </span>
+                                <span className="owner-rate">{gig.owner.rate}</span>
+                                <span className="owner-number-rates">({seller.reviews.length * 11 + 103})</span>
+                                <span className="divider">|</span>
+                                <span className="queue">10 Orders in Queue</span>
                             </div>
                         </div>
                     </div>
-                </section>
-            </CtaModal>
-
-            <section className="gig-details full main-layout">
-                <BreadCrumbs category={gig.category} />
-                <CallToAction gig={gig} addToCart={addToCart} setPackage={setPackage} openModal={openModal} />
-                <section className="top-details container full main-layout">
-                    <div className="owner-details-container">
-                        <h1 className="gig-title">{gig.title}</h1>
-                        <div className="profile-container">
-                            <img src={gig.owner.imgUrl} alt="owner-img" className="owner-profile-img-meduim" onClick={scrollToUserInfo} onError={e => e.currentTarget.src = defaultUserImg} />
-                            <div className="owner-details">
-                                <div className="user-container" onClick={scrollToUserInfo}>
-                                    <h3 className="user-title">{gig.owner.fullname}</h3>
-                                    <span className="username">@{seller.username}</span>
-                                </div>
-                                <div className="star-wrapper" onClick={scrollToReview}>
-                                    <span className="star-svg">
-                                        <img src={starGrey} alt="star-svg" className="star" />
-                                    </span>
-                                    <span className="owner-rate">{gig.owner.rate}</span>
-                                    <span className="owner-number-rates">({seller.reviews.length * 11 + 103})</span>
-                                    <span className="divider">|</span>
-                                    <span className="queue">10 Orders in Queue</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-container">
-                            <Carousel infiniteLoop={true} showIndicators={false} thumbWidth={'100px'} showStatus={false} renderArrowNext={(clickHandler, hasNext) => {
+                    <div className="carousel-container">
+                        <Carousel infiniteLoop={true} showIndicators={false} thumbWidth={'100px'} showStatus={false} renderArrowNext={(clickHandler, hasNext) => {
+                            return (
+                                <button className='next-btn arrow' onClick={clickHandler} >
+                                    <i className="fa-solid fa-chevron-right"></i>
+                                </button>
+                            )
+                        }}
+                            renderArrowPrev={(clickHandler, hasNext) => {
                                 return (
-                                    <button className='next-btn arrow' onClick={clickHandler} >
-                                        <i className="fa-solid fa-chevron-right"></i>
+                                    <button className='prev-btn arrow' onClick={clickHandler} >
+                                        <i className="fa-solid fa-chevron-left"></i>
                                     </button>
                                 )
                             }}
-                                renderArrowPrev={(clickHandler, hasNext) => {
-                                    return (
-                                        <button className='prev-btn arrow' onClick={clickHandler} >
-                                            <i className="fa-solid fa-chevron-left"></i>
-                                        </button>
-                                    )
-                                }}
-                            >
-                                {!gig.imgUrls.length ? <img src={defaultImgUrl} onError={e => e.currentTarget.src = defaultGigImg} /> : gig.imgUrls.map(img => <img key={img} src={img} onError={e => e.currentTarget.src = defaultGigImg} />)}
-                            </Carousel>
-                        </div>
+                        >
+                            {!gig.imgUrls.length ? <img src={defaultImgUrl} onError={e => e.currentTarget.src = defaultGigImg} /> : gig.imgUrls.map(img => <img key={img} src={img} onError={e => e.currentTarget.src = defaultGigImg} />)}
+                        </Carousel>
                     </div>
-                </section>
-
-
-
-
-                <div className="about-gig">
-                    <h1 className="gig-about-title">About this gig</h1>
-                    <p className="gig-description">{gig.description}</p>
-                </div>
-                <div className="user-info" ref={userInfoRef}>
-                    <h1 className="about-seller">About the seller</h1>
-                    <UserMiniDetail gig={gig} />
-                </div>
-                <div className="user-reviews" ref={userReviewRef}>
-                    <ReviewList gigOwnerId={gig.owner._id} />
                 </div>
             </section>
-        </>
+
+            <div className="about-gig">
+                <h1 className="gig-about-title">About this gig</h1>
+                <p className="gig-description">{gig.description}</p>
+            </div>
+            <div className="user-info" ref={userInfoRef}>
+                <h1 className="about-seller">About the seller</h1>
+                <UserMiniDetail gig={gig} />
+            </div>
+            <div className="user-reviews" ref={userReviewRef}>
+                <ReviewList gigOwnerId={gig.owner._id} />
+            </div>
+        </section>
     )
 }
